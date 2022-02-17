1d6f9a8e8e20110276f14cb3f89002486431f112
ggydush-fn <greg.gydush@freenome.com>
2022-02-16 22:46:29 -0800

    Cleaner

--- install.sh
+++ install.sh
@@ -1,5 +1,5 @@
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended && rm $HOME/.zshrc
 git clone https://github.com/sindresorhus/pure.git "$HOME/.zsh.d/pure"
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
-git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf/ && ~/.fzf/install --all --no-bash
+git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf/ && ~/.fzf/install --all --no-update-rc --no-bash --no-fish
 ln -s $HOME/.zsh.d/.zshenv $HOME/.zshenv
