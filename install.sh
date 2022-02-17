5cec8c3f81cce2641bfd1971cb0894ae3d1252ee
ggydush-fn <greg.gydush@freenome.com>
2022-02-16 22:22:30 -0800

    Update install to use unattended

--- install.sh
+++ install.sh
@@ -1,4 +1,4 @@
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
 git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf/ && ~/.fzf/install
 git clone https://github.com/sindresorhus/pure.git "$HOME/.zsh.d/pure"
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
